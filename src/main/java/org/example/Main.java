99e7b7a47d8ebd8fcc205f5b5105a6262a4d97ed
Denis <denisghera@yahoo.com>
2023-11-14 15:00:21 +0200

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        InputDevice id = new InputDevice(System.in);
+        OutputDevice od = new OutputDevice(System.out);
+
+        if(args.length < 1) od.print("No valid option selected!");
+        else if (args[0].equals("start")) {
+            if (args.length < 3) od.print("No account specified!");
+            else {
+                Application app = new Application(id, od);
+                app.startApplication(args[1], args[2]);
+            }
+        }
+        else if (args[0].equals("test")) {
+            od.print("Testing 123!");
+        }
     }
 }
